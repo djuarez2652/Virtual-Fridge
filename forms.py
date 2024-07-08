6087c9d69f8602287c09883b2a292dcee3e8d5d7
sonialiao <sonialiao131@hotmail.com>
2024-07-08 17:19:52 -0400

    combined forms scripts into 1 python file

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
+from wtforms import StringField, PasswordField, SubmitField, DateField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
@@ -12,10 +12,17 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password',
                              validators=[DataRequired(), Length(min=8, max=20)])
     submit = SubmitField('Login')
+
+class addStockForm(FlaskForm):
+    item_name = StringField('Item Name',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    expire_date = DateField('Expiration Date',
+                           validators=[DataRequired()])
+    add_btn = SubmitField('Add')
+
