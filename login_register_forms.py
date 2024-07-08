a9efa004acbf679f4ab2872532e4d7c374ebe131
djuarez2652 <85270302+djuarez2652@users.noreply.github.com>
2024-07-08 16:08:07 -0400

    created register and login flask forms

---
+++ login_register_forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8, max=20)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8, max=20)])
+    submit = SubmitField('Login')
