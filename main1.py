491aa6a27ca4ff3481b66b286ce475ba5fb041f5
sonialiao <sonialiao131@hotmail.com>
2024-07-09 16:05:24 -0400

    implemented delete button for food items in stock.html, added display of stock for recipes.html

--- main1.py
+++ main1.py
@@ -91,10 +91,18 @@ def stock():
         return redirect(url_for("stock"))
     return render_template('stock.html', form=add_stock_form, stock_query=query_stock(current_user.id))
 
+@app.route("/remove_stock", methods=['GET'])
+def remove_stock():
+  item_name = request.args['item_name']
+
+  remove_food(current_user.id, item_name)
+
+  
+  return redirect(url_for('stock'))
 
 @app.route("/recipes")
 def recipes():
-  return render_template('recipes.html', subtitle='Recipe page', text='Generate Recipes')
+  return render_template('recipes.html', stock_query=query_stock(current_user.id))
 
 
 # add a food item to database
@@ -111,9 +119,19 @@ def has_food(user_id, food_name):
         return food_item
     return None
 
+# pulls up all of the stock corresponding to a user
 def query_stock(user_id):
     result = db.session.execute(db.select(Stock.food_name, Stock.expiration_date).where(Stock.user_id == user_id)).all()
     return result
+
+# removes a food item with a user's id from the stock table
+def remove_food(user_id, food_name):
+  to_remove = Stock.query.filter_by(user_id=user_id, food_name=food_name).first()
+  if to_remove:
+      msg_text = 'Food item %s successfully removed' % str(to_remove)
+      db.session.delete(to_remove)
+      db.session.commit()
+      print(msg_text)
 
 if __name__ == '__main__':         \
     app.run(debug=True, host="0.0.0.0")
