b3fe1a0134f60ca1f79085d3beed9db3eed85db7
Doug Bogle <dbogle@bowdoin.edu>
2024-07-10 18:49:53 +0000

    fixing up auto testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 import unittest, sys, os
 
-sys.path.append('../seo-week-4')
+#sys.path.append('../seo-week-4')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main1 import app, db
 
 class UsersTests(unittest.TestCase):
@@ -23,6 +24,14 @@ class UsersTests(unittest.TestCase):
                                       confirm_password=password),
                             follow_redirects=True)
 
+
+    def login(self, username, password):
+        return self.app.post('/login',
+                             data=dict(username=username,
+                                       password=password),
+                             follow_redirects=True)
+
+
     def test_valid_user_registration(self):
         response = self.register('test', 'FlaskIsAwesome')
         self.assertEqual(response.status_code, 200)
@@ -34,21 +43,6 @@ class UsersTests(unittest.TestCase):
     def test_invalid_password_registration(self):
         response = self.register('test2', '1')
         self.assertIn(b'Field must be between 8 and 20 characters long.', response.data)
-
-
-    def add_stock(self, item_name, expire_date):
-        return self.app.post('/stock',
-                             data=dict(item_name=item_name,
-                                       expire_date=expire_date),
-                             follow_redirects=True)
-
-    def test_valid_item(self):
-        response = self.add_stock('Apple', '10-12-2024')
-        self.assertEqual(response.status_code, 200)
-
-    def test_invalid_item_name(self):
-        response = self.add_stock('aa', '10-12-2024')
-        self.assertIn(b'Field must be between 2 and 20 characters long.', response.data)
 
 
 if __name__ == "__main__":
